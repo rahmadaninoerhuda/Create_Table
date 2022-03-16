ee9ba432f253ad998fd6c7aac13151caa381d2fa
RahmadaniNurhuda <rahmadani.nurhuda@gmail.com>
2022-03-16 14:19:44 +0700

    connect sql

---
+++ Create_Table/Create_Table/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace Koneksi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Connecting();
+        }
+
+        public void Connecting()
+        {
+            using (
+                SqlConnection con = new SqlConnection("data source=LAPTOP-I5CF3Q9T;" +
+                "database=ProdiTI;User id=sa;Password=dedekgemes123")
+                )
+            {
+                con.Open();
+                Console.WriteLine("Berhasi Terhubung ke Database:");
+            }
+        }
+    }
+}
+
+
